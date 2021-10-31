29da8e54947325cad6fdb04eea49517682bbbf27
Homi Mickey <homimickey@gmail.com>
2021-10-31 09:15:10 +0530

    added all files required

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    /* options */
+});
+let colorGlobal = "#fff";
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    console.log(colorGlobal);
+    socket.emit("conn", colorGlobal);
+
+    socket.on("change", (color) => {
+        console.log(color);
+        colorGlobal = color;
+        io.emit("changed", color);
+    });
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public" + "/index.html");
+});
+
+app.get("/light", (req, res) => {
+    res.sendFile(__dirname + "/public" + "/color.html");
+});
+
+httpServer.listen(3000, () => {
+    console.log("listening on port 3000");
+});
